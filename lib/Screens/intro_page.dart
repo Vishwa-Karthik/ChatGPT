e21bb5100360cd1dd24c16f3574b06edc09ad8bd
Vishwa-Karthik <vishwa.prarthana@gmail.com>
2023-02-13 09:32:02 +0530

    intro_page

--- lib/Screens/intro_page.dart
+++ lib/Screens/intro_page.dart
@@ -12,107 +12,105 @@ class IntroPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: KScaffoldBackgroundColor,
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //* logo
-            Padding(
-              padding: const EdgeInsets.only(
-                  top: 50.0, bottom: 0, right: 80, left: 80),
-              child: Image.asset(
-                'assets/chat-bot.png',
-                height: 250,
-              ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //* logo
+          Padding(
+            padding: const EdgeInsets.only(
+                top: 50.0, bottom: 0, right: 80, left: 80),
+            child: Image.asset(
+              'assets/chat-bot.png',
+              height: 250,
             ),
+          ),
 
-            const SizedBox(
-              height: 10,
-            ),
+          const SizedBox(
+            height: 10,
+          ),
 
-            //* Tagline
-            Padding(
-              padding: const EdgeInsets.only(left: 25, right: 20),
-              child: Text(
-                "Lean Back, I'm Intelligent human like manner chatbot.",
-                textAlign: TextAlign.center,
-                style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.w300,
-                  fontSize: 30,
-                  letterSpacing: 3,
-                  color: KTextColor,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 25, right: 20),
-              child: Text(
-                "Not your regualar AI Chat Bot ",
-                textAlign: TextAlign.center,
-                style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.w300,
-                  fontSize: 30,
-                  letterSpacing: 3,
-                  color: KTextColor,
-                ),
-              ),
-            ),
-
-            const SizedBox(
-              height: 25,
-            ),
-
-            //* Tagline
-            Text(
-              "Open AI Chat Bot",
+          //* Tagline
+          Padding(
+            padding: const EdgeInsets.only(left: 25, right: 20),
+            child: Text(
+              "Lean Back, I'm Intelligent human like manner chatbot.",
+              textAlign: TextAlign.center,
               style: GoogleFonts.poppins(
-                letterSpacing: 2,
-                fontSize: 23,
+                fontWeight: FontWeight.w300,
+                fontSize: 30,
+                letterSpacing: 3,
                 color: KTextColor,
               ),
             ),
-            const SizedBox(
-              height: 20,
-            ),
-
-            //* Get STarted button
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.circular(35),
-              ),
-              child: MaterialButton(
-                onPressed: () {
-                  HapticFeedback.mediumImpact();
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) {
-                    return const HomePage();
-                  }));
-                },
-                child: Text(
-                  "Get Started",
-                  style:
-                      GoogleFonts.notoSerif(color: Colors.white, fontSize: 20),
-                ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 25, right: 20),
+            child: Text(
+              "Not your regualar AI Chat Bot ",
+              textAlign: TextAlign.center,
+              style: GoogleFonts.poppins(
+                fontWeight: FontWeight.w300,
+                fontSize: 30,
+                letterSpacing: 3,
+                color: KTextColor,
               ),
             ),
+          ),
 
-            const SizedBox(
-              height: 40,
+          const SizedBox(
+            height: 25,
+          ),
+
+          //* Tagline
+          Text(
+            "Open AI Chat Bot",
+            style: GoogleFonts.poppins(
+              letterSpacing: 4,
+              fontSize: 23,
+              fontWeight: FontWeight.w600,
+              color: KTextColor,
             ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
 
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 28.0),
-              child: Divider(
-                color: Colors.white,
+          //* Get STarted button
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(35),
+            ),
+            child: MaterialButton(
+              onPressed: () {
+                HapticFeedback.mediumImpact();
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) {
+                  return const HomePage();
+                }));
+              },
+              child: Text(
+                "Get Started",
+                style: GoogleFonts.notoSerif(color: Colors.white, fontSize: 20),
               ),
             ),
-            Text(
-              "Developed by Vishwa Karthik",
-              style: GoogleFonts.notoSans(color: Colors.white, fontSize: 12),
+          ),
+
+          const Spacer(),
+
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 28.0),
+            child: Divider(
+              color: Colors.white,
             ),
-          ],
-        ),
+          ),
+          Text(
+            "Developed by Vishwa Karthik",
+            style: GoogleFonts.notoSans(color: Colors.white, fontSize: 12),
+          ),
+
+          const Spacer(),
+        ],
       ),
     );
   }
