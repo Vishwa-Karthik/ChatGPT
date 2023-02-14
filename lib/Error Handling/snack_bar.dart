40655e3e899e2595cc15207eb320b5c60c9512be
Vishwa-Karthik <vishwa.prarthana@gmail.com>
2023-02-14 13:20:47 +0530

    error handling

--- lib/Error Handling/snack_bar.dart
+++ lib/Error Handling/snack_bar.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:motion_toast/motion_toast.dart';
 
-void showErrorToast(
-  context,
-  errorMessage,
-) {
+void showErrorToast(context, errorMessage) {
   MotionToast.error(
     description: Text(errorMessage),
     toastDuration: const Duration(milliseconds: 450),
