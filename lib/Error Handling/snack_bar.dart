ccf4595b461e7e452ff75817e461aa9f47a26532
Vishwa-Karthik <vishwa.prarthana@gmail.com>
2023-02-16 08:08:27 +0530

    fix

--- lib/Error Handling/snack_bar.dart
+++ lib/Error Handling/snack_bar.dart
@@ -3,6 +3,8 @@ import 'package:motion_toast/motion_toast.dart';
 
 void showErrorToast(context, errorMessage) {
   MotionToast.error(
+    height: MediaQuery.of(context).size.height * 0.1,
+    width: MediaQuery.of(context).size.width * 0.7,
     description: Text(errorMessage),
     toastDuration: const Duration(milliseconds: 450),
     enableAnimation: true,
